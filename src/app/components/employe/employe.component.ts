44f7a2310727923aa53af8bd6648fa9627a5320b
Isabel <isabelpastorlopezcv@gmail.com>
2023-01-24 21:26:24 +0100

    ejercicio angular

---
+++ src/app/components/employe/employe.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeesService } from 'src/app/services/employees.service';
+
+@Component({
+  selector: 'app-employe',
+  templateUrl: './employe.component.html',
+  styleUrls: ['./employe.component.css']
+})
+export class EmployeComponent implements OnInit {
+
+  employe: any = {}; 
+  constructor(
+   
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/employees/employees.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeesService } from 'src/app/services/employees.service';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  
+
+  constructor() { 
+    
+  }
+
+  
+
+  ngOnInit(): void {
+  }
+
+}
